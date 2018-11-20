cb2d127d517d6fef7b843f69600b2c8cdcb42612
Sumnimarana1 <33071134+Sumnimarana1@users.noreply.github.com>
2018-11-19 22:33:09 -0600

    Update rana_reducer.py
    Added comments to reducer.py

--- rana/rana_reducer.py
+++ rana/rana_reducer.py
@@ -1,22 +1,27 @@
-salesTotal = 0
+import sys
+#initializing all variables
+    salesTotal = 0
     count = 0
     oldKey = None
 
+    #for each line in file
     for line in sys.stdin:
+        #split the line at tab delimeter
         data =line.strip().split("\t")
-
+        #if data doesnot have length of two continues;
         if len(data) != 2:
             continue
         
+        #stores the value
         thisKey, thisSale = data
-
+            
+        #check for the old key
         if oldKey and oldKey != thisKey:
-            print "{0}\t{1}".format(oldKey, salesTotal/count)
-            count = 0
-            salesTotal = 0
-
-        oldKey = thisKey
+            print "{0}\t{1}".format(oldKey, salesTotal/count) #prints the key and average pairs
+            count = 0 #rechanges the count to zero
+            salesTotal = 0 #rechanges the total to zero
+        
+        oldKey = thisKey #makes the old key as new key
         count += 1
         salesTotal += float(thisSale)
-
-print "{0}\t{1}".format(oldkey,salesTotal)
+    print "{0}\t{1}".format(oldkey,salesTotal) #prints the end key value pair
