522af4d3c98a7ca8ae1dc6bbc81f8d89cb9d7c27
Sumnimarana1 <33071134+Sumnimarana1@users.noreply.github.com>
2018-11-19 21:48:44 -0600

    Update rana_mapper.py
    Added comments to mapper.py

--- rana/rana_mapper.py
+++ rana/rana_mapper.py
@@ -1,10 +1,14 @@
 import sys
+    #reads for the each line in the file
     for line in sys.stdin:
+        #splits the data by tab-delimeter
         data = line.strip().split("\t")
+        #each data is mapped
         ID,BATHRM,HF_BATHRM,HEAT,AC,NUM_UNITS,ROOMS,BEDRM,AYB,YR_RMDL,EYB,STORIES,SALEDATE,PRICE,QUALIFIED,SALE_NUM,GBA,BLDG_NUM,STYLE,STRUCT,GRADE,CNDTN,EXTWALL,ROOF,INTWALL,KITCHENS,FIREPLACES,USECODE,LANDAREA,GIS_LAST_MOD_DTTM,SOURCE,CMPLX_NUM,LIVING_GBA,FULLADDRESS,CITY,STATE,ZIPCODE,NATIONALGRID,LATITUDE,LONGITUDE,ASSESSMENT_NBHD,ASSESSMENT_SUBNBHD,CENSUS_TRACT,CENSUS_BLOCK,WARD,SQUARE,X,Y,QUADRANT
 = data
-
+       #here spliting the date in order to get month only
         newdata = SALEDATE.strip().split("/")
+        #check if there is valid date given
         if len(newdata) == 3:
                 Month, Day,year =newdata
-                print Month,"\t", PRICE
+                print Month,"\t", PRICE #prints the month and price
