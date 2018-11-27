a94976b907bb6733889ae5e0fe41786b8fb1e111
Kyle Thompson <kyyservus@gmail.com>
2018-11-26 18:44:11 -0600

    Comments added

--- thompson/mapper.py
+++ thompson/mapper.py
@@ -1,13 +1,15 @@
 #Mapper for Realestate Data
 import sys
 
-for line in sys.stdin:
+for line in sys.stdin:		#reads each line in the system
 
     data = line.strip().split("\t")
-    if len(data) == 49:
+    if len(data) == 49:		#checks to ensure correct data and split used
 
         ID,BATHRM,HF_BATHRM,HEAT,AC,NUM_UNITS,ROOMS,BEDRM,AYB,YR_RMDL,EYB,STORIES,SALEDATE,PRICE,QUALIFIED,SALE_NUM,GBA,BLDG_NUM,STYLE,STRUCT,GRADE,CNDTN,EXTWALL,ROOF,INTWALL,KITCHENS,FIREPLACES,USECODE,LANDAREA,GIS_LAST_MOD_DTTM,SOURCE,CMPLX_NUM,LIVING_GBA,FULLADDRESS,CITY,STATE,ZIPCODE,NATIONALGRID,LATITUDE,LONGITUDE,ASSESSMENT_NBHD,ASSESSMENT_SUBNBHD,CENSUS_TRACT,CENSUS_BLOCK,WARD,SQUARE,X,Y,QUADRANT
 = data
+				#assigns header names to each different colum in data
+
         print "{0}\t{1}".format(PRICE, SQUARE)
 
 
--- thompson/reducer.py
+++ thompson/reducer.py
@@ -1,17 +1,19 @@
-#Reducer for Realestate Data
+#Reducer and computer for Realestate Data
 
 import sys
 
-totalcost = 0
-totasqr = 0
+totalcost = 0	#Counter for Total Cost
+totasqr = 0	#Counter for total square feet
 
-for line in sys.stdin:
-    data = line.strip().split("\t")
-    if len(data) = 2:
+for line in sys.stdin:		#reads through input file line by line
+    data = line.strip().split("\t")	#assigns line to list called data
+    if len(data) = 2:			#checks to make sure correct data is being used
         cost, sqr = data
 
-        if(cost != " " and cost != "COST":
+        if(cost != " " and cost != "COST":	#ensures the Header Line isn't computed,
+						#and that sqrfeet of blank costs aren't added
             totalcost = totalcost + float(cost)
             totasqr = totasqr + float(sqr)
-avg = totalcost/totasqr
-print("{0}".format(avg))
+
+avg = totalcost/totasqr		#computes avg cost per square foot
+print("{0}".format(avg))	#prints avg to system
