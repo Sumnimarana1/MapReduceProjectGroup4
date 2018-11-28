577c559b2cffe1e80fb330c062305def383ec178
sumnimarana1 <sumnimarana1@gmail.com>
2018-11-27 21:29:40 -0600

    Added the comments to the Mapper, sort and reducer.

--- rana/LocalTestedFiles/mapper.py
+++ rana/LocalTestedFiles/mapper.py
@@ -1,21 +1,29 @@
-#this is to make the new file with just datas as correct column
+#this is to open the text files to read
 inputFile = open("DC.txt", "r")
+#this is to open the file to write the output to
 outputFile = open("MapperOutput.txt", "w")
 count =1
+#start the for loop and goes through each line in the file
 for line in inputFile:
+    #strips and splits the data at the tab-delimiter
     data = line.strip().split("\t")
     count += 1
+    # check for the bad input
     if len(data) == 49:
+        #values of all 49 columns
         ID,BATHRM,HF_BATHRM,HEAT,AC,NUM_UNITS,ROOMS,BEDRM,AYB,YR_RMDL,EYB,STORIES,SALEDATE,PRICE,QUALIFIED,SALE_NUM,GBA,BLDG_NUM,STYLE,STRUCT,GRADE,CNDTN,EXTWALL,ROOF,INTWALL,KITCHENS,FIREPLACES,USECODE,LANDAREA,GIS_LAST_MOD_DTTM,SOURCE,CMPLX_NUM,LIVING_GBA,FULLADDRESS,CITY,STATE,ZIPCODE,NATIONALGRID,LATITUDE,LONGITUDE,ASSESSMENT_NBHD,ASSESSMENT_SUBNBHD,CENSUS_TRACT,CENSUS_BLOCK,WARD,SQUARE,X,Y,QUADRANT= data
-        newdata = SALEDATE.strip().split("/")
-        if len(newdata) == 3:
+        newdata = SALEDATE.strip().split("/") #splits the Data at '/' 
+        if len(newdata) == 3: # if the date is valid
+            #seperates as Month, Day and year
             Month, Day, year = newdata
-            if int(Month) < 10:
-                Month = "0" + Month
-             if(PRICE != "" and float(PRICE) !=0):
-                outputFile.write("{0}\t{1}".format(Month,PRICE))
+            if int(Month) < 10: # if the month is less that 10
+                Month = "0" + Month #adding the prefix in the month as 01,02..11,12
+            #ignores the bad input( if the price is empty)
+             if(PRICE != "" and float(PRICE) !=0): 
+                outputFile.write("{0}\t{1}".format(Month,PRICE)) # write to the file
                 print Month,"\t", PRICE
                 outputFile.write("\n")
 print(count)
+#closing the file
 inputFile.close()
 outputFile.close()
--- rana/LocalTestedFiles/sort.py
+++ rana/LocalTestedFiles/sort.py
@@ -1,10 +1,13 @@
-#thisis to create a new text file with sorted store and cost
-
+#thisis to create a new text file with sorted month and Price
+#this is to open the text files to read
 inputFile2 = open("MapperOutput.txt", "r")
+#this is to open the file to write the output to
 outputFile2 = open("Sortoutput.txt", "w")
 lines = inputFile2.readlines()
 lines.sort()
+#going through each line
 for line in lines:
-   outputFile2.write(line)
+   outputFile2.write(line) #sorts the line
+#closing the file
 inputFile2.close()
 outputFile2.close()
