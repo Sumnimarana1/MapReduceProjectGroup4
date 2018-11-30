35cf803b5657e9bbc285f6494d815941497e3450
Goutham Neravetla <turingloop@gmail.com>
2018-11-29 19:35:23 -0600

    probably final commit

--- neravetla/localtest/mapper.py
+++ neravetla/localtest/mapper.py
@@ -8,18 +8,18 @@ writefile = open("MAPPED.txt","w")
 countLine = 0
 
 for line in readfile:
-    data = line.strip().split("\t")
+    data = line.strip().split("\t") #this remove any white space, and turns the rest into a list
     countLine += 1
-    if len(data) == 49:
+    if len(data) == 49:#checks if it is the right input
         ID,BATHRM,HF_BATHRM,HEAT,AC,NUM_UNITS,ROOMS,BEDRM,AYB,YR_RMDL,EYB,STORIES,SALEDATE,PRICE,QUALIFIED,SALE_NUM,GBA,BLDG_NUM,STYLE,STRUCT,GRADE,CNDTN,EXTWALL,ROOF,INTWALL,KITCHENS,FIREPLACES,USECODE,LANDAREA,GIS_LAST_MOD_DTTM,SOURCE,CMPLX_NUM,LIVING_GBA,FULLADDRESS,CITY,STATE,ZIPCODE,NATIONALGRID,LATITUDE,LONGITUDE,ASSESSMENT_NBHD,ASSESSMENT_SUBNBHD,CENSUS_TRACT,CENSUS_BLOCK,WARD,SQUARE,X,Y,QUADRANT = data
-        if countLine >= 2:
-            if (int(ROOMS) == 0) or (int(BATHRM) == 0):
+        if countLine >= 2:#ignores the header
+            if (int(ROOMS) == 0) or (int(BATHRM) == 0):#removes invalid data
                 continue
-            elif (int(ROOMS) < 10):
+            elif (int(ROOMS) < 10):#adds 0 to key value that helps with proper sorting later
                 ROOMS = "0" + ROOMS
-                writefile.write("{0}\t{1}\n".format(ROOMS,BATHRM))
+                writefile.write("{0}\t{1}\n".format(ROOMS,BATHRM))#write to file
             else:
                 writefile.write("{0}\t{1}\n".format(ROOMS,BATHRM))
 print(countLine)
-readfile.close()
+readfile.close()#closed file IO
 writefile.close()
--- neravetla/localtest/sort.py
+++ neravetla/localtest/sort.py
@@ -1,15 +1,12 @@
+'''Reads output from mapper.py, sorts it, and prints it to standard output
+which can be read by reducer'''
 
 #opens mapped.txt in read mode
 inputFile = open("MAPPED.txt", "r")
 #opens sorted.txt in write mode
 outputFile = open("SORTED.txt", "w")
 
-#reading the first line and writing it seperately because it is a header. It needs to be excluded.
-#header = inputFile2.readline()
-#outputFile2.write(header)
 
-#read after the first line(we are exclusing the headline)
-#lines = inputFile2.readlines()[1:]
 lines = inputFile.readlines()
 lines.sort()
 for line in lines:
