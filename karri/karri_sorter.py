edd01a1513961c4163f2c4c48734f61d64696a54
KrishnaVeni Karri <s530471@nwmissouri.edu>
2018-12-04 16:02:33 -0600

    removed sorter

--- karri/karri_sorter.py
+++
@@ -1,9 +0,0 @@
-tempfile = open("tweety.txt","r")  # open file, read-only
-sorted_tweety = open("sorted_tweety.txt", "w") # open file, write
-lines = tempfile.readlines()
-lines.sort()
-
-for line in lines:
-	sorted_tweety.write(line)
-tempfile.close()
-sorted_tweety.close()
